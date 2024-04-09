20107c822d783897e44dc3a911a3b4c4325c516f
namphong68 <namphong68@gmail.com>
2024-04-09 15:39:32 +0700

    commit note

--- MvcMovie/obj/Debug/net7.0/MvcMovie.AssemblyInfo.cs
+++ MvcMovie/obj/Debug/net7.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+993b66e210b69cb11746c0412d0d901659d3cd30")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
